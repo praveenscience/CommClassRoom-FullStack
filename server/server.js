df39c728b5fea71dcfe521fc9c9046df5e3395d7
Praveen Kumar <praveenscience@gmail.com>
2021-09-20 17:39:55 +0100

    Created our first route.

--- server/server.js
+++ server/server.js
@@ -5,6 +5,11 @@ const app = express();
 // Configure a port address.
 const port = 5000;
 
+// Create a Get Request handler for /.
+app.get("/", (req, res) => {
+  res.json("Welcome to Community Classroom!");
+});
+
 // Make the app listen to a port.
 app.listen(port, () => {
   console.log(`Server started in port ${port}.`);
