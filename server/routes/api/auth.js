8e20aec3553575230f33ec0e5f5c41ac30fffd70
Praveen Kumar <praveenscience@gmail.com>
2021-09-21 17:05:44 +0100

    Moved users to its own constants.

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -4,31 +4,6 @@ const express = require("express");
 const app = express.Router();
 
 // Temporary Users Data.
-const Users = {
-  Praveen: {
-    Name: "Praveen Kumar Purushothaman",
-    // WARNING: USE HASHED PASSWORD!!!
-    Password: "Praveen",
-    Type: "Admin", // Student, Mentor, Admin, Disabled
-    Verified: true,
-    Personal: {
-      Email: "commclassroom@praveen.science",
-      Phone: "+447894561230"
-    },
-    CreatedAt: new Date()
-  },
-  Rohan: {
-    Name: "Rohan Kulkarni",
-    // WARNING: USE HASHED PASSWORD!!!
-    Password: "Rohan",
-    Type: "Student", // Student, Mentor, Admin, Disabled
-    Verified: false,
-    Personal: {
-      Email: "rohan@gmail.com",
-      Phone: "+919876543210"
-    },
-    CreatedAt: new Date()
-  }
-};
+const Users = require("../../constants/Users.json");
 // Export the Router.
 module.exports = app;
