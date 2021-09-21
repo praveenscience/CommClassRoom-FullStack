defa11022ae542a5812fa273eb2ffdefbe18f858
Praveen Kumar <praveenscience@gmail.com>
2021-09-21 16:43:59 +0100

    Created API route.

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,16 +2,11 @@
 const express = require("express");
 // Create a Router App.
 const app = express.Router();
-// Import the routes.
-const api = require("./api");
 
 // Create a Get Request handler for /.
 app.get("/", (req, res) => {
-  res.json("Welcome to Community Classroom!");
+  res.json("Welcome to Community Classroom API!");
 });
-
-// API routes handler.
-app.use("/api", api);
 
 // Export the Router.
 module.exports = app;
