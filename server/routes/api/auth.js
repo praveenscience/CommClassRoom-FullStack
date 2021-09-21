17a82cf1ea15bce6bd7b205c60a78a6cf659a9d5
Praveen Kumar <praveenscience@gmail.com>
2021-09-21 17:29:38 +0100

    Setup the endpoint for /login.

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,11 +1,12 @@
 // Import Express JS library.
 const express = require("express");
+const { CheckUserNamePassword } = require("../../helpers/Users");
 // Create a Router App.
 const app = express.Router();
 
 let CurrentUser = null;
 
-// Setup Login Path.
+// Endpoint to check current logged in user.
 app.get("/login", (req, res) => {
   if (CurrentUser) {
     res.json(CurrentUser);
@@ -15,6 +16,37 @@ app.get("/login", (req, res) => {
     });
   }
 });
-
+// Endpoint to login a user.
+app.post("/login", (req, res) => {
+  const { Username, Password } = req.body;
+  if (!Username || !Password) {
+    res.status(400).json({
+      Error: "Both Username and Password fields are mandatory."
+    });
+  } else if (typeof Username === "string" && typeof Password === "string") {
+    switch (CheckUserNamePassword(Username, Password)) {
+      case 0:
+        res.status(404).json({
+          Error: "User does not exist."
+        });
+        CurrentUser = null;
+        break;
+      case -1:
+        res.status(400).json({
+          Error: "Invalid username or password."
+        });
+        CurrentUser = null;
+        break;
+      default:
+        CurrentUser = CheckUserNamePassword(Username, Password);
+        res.json(CurrentUser);
+    }
+  } else {
+    res.status(400).json({
+      Error:
+        "Both Username and Password fields are supposed to be string values."
+    });
+  }
+});
 // Export the Router.
 module.exports = app;
