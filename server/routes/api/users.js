a7877c75448e5d3f963c4bb6924b5be5f8dc0875
Praveen Kumar <praveenscience@gmail.com>
2021-09-22 17:15:34 +0100

    List all the users endpoint done.

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const { ListAllUsers } = require("../../helpers/Users");
 // Create a Router App.
 const app = express.Router();
+
+// All users without password!
+const AllUsersObject = ListAllUsers();
+const AllUsers = Object.keys(AllUsersObject).map(Username => {
+  const CurUser = {
+    Username,
+    ...AllUsersObject[Username]
+  };
+  delete CurUser.Password;
+  return CurUser;
+});
+
+// List all the users endpoint.
+// WARNING: PROTECT THIS ROUTE.
+app.get("/", (req, res) => {
+  res.json(AllUsers);
+});
 
 // Export the Router.
 module.exports = app;
