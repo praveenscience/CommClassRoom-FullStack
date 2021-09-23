2a318ecdd9d3731aca86b3efdacc71cef184ca34
Praveen Kumar <praveenscience@gmail.com>
2021-09-23 16:45:33 +0100

    Initialised and implemented Express Sessions.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 // Import the Express JS Framework.
 const express = require("express");
+// Import Sessions.
+const session = require("express-session");
 // Initialising an app instance.
 const app = express();
 // Configure a port address.
@@ -10,6 +12,22 @@ const root = require("./routes/root");
 // Add some custom middleware.
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Create a session config object.
+const SessionConfig = {
+  secret: "c0mmun!7y cl@ssr00m",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
+};
+// If running in prod, do some extra secure stuff.
+// I am not even sure if the below condition shows true.
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
+// Initialise the middleware.
+app.use(session(SessionConfig));
 
 // Add the routes as middleware - route handler.
 app.use("/", root);
