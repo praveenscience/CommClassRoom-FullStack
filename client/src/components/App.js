fdca0561367ecc4dca08adc1dada0aa9145526c9
Praveen Kumar <praveenscience@gmail.com>
2021-09-24 17:21:32 +0100

    Implemented Header in the App.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import Header from "./Bootstrap/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Community Classroom lies here...</h1>
+        <Header className="justify-content-center" dark={true}>
+          Community Classroom
+        </Header>
       </div>
     );
   }
