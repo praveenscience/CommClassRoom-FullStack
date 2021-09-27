40c97eb71b12e09dcad0ec7685d8dca99d6bcc24
Praveen Kumar <praveenscience@gmail.com>
2021-09-27 17:27:38 +0100

    Added the role part of the registration.

--- client/src/components/Home/Register.js
+++ client/src/components/Home/Register.js
@@ -1,6 +1,8 @@
 import FormGroup from "../Bootstrap/FormGroup";
 
 const Register = ({ Card, Forms, Errors }) => {
+  const FormElements = { ...Forms };
+  delete FormElements.Role;
   return (
     <div className="Register">
       <Card
@@ -8,7 +10,7 @@ const Register = ({ Card, Forms, Errors }) => {
         Text="Please use this form to register for a new account."
       >
         <form>
-          {Object.keys(Forms).map(fg => (
+          {Object.keys(FormElements).map(fg => (
             <FormGroup
               Id={"Reg" + fg}
               Type={fg === "Password" ? "password" : "text"}
@@ -16,6 +18,14 @@ const Register = ({ Card, Forms, Errors }) => {
               Label={fg}
             />
           ))}
+          <div className="form-group mb-3">
+            <label htmlFor="Role">Role</label>
+            <select class="form-select" aria-label="Select your Role">
+              <option value=""></option>
+              <option value="Student">Student</option>
+              <option value="Mentor">Mentor</option>
+            </select>
+          </div>
           <button type="submit" class="btn btn-primary">
             Register
           </button>
