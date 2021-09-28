dd5874553b5d6f63c5baecd1d9159e24d03ab520
Praveen Kumar <praveenscience@gmail.com>
2021-09-28 17:29:08 +0100

    Created a service call for Register.

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -2,3 +2,6 @@ import Axios from "axios";
 
 export const LoginUser = (Username, Password) =>
   Axios.post("/api/auth/login", { Username, Password });
+
+export const RegisterUser = (Username, Password, Name, Email, Role) =>
+  Axios.post("/api/auth/register", { Username, Password, Name, Email, Role });
