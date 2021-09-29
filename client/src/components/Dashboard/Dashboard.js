fd5be86915346fe47704346f78200be75dd94fbf
Praveen Kumar <praveenscience@gmail.com>
2021-09-29 17:15:39 +0100

    Created a form to verify users.

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,12 +1,30 @@
+import FormGroup from "../Bootstrap/FormGroup";
+
 const Dashboard = ({ Card, handleLogout, User }) => {
   return (
     <Card Title={`Welcome ${User.Name}`}>
-      <p>
-        Welcome to Community Classroom.{" "}
-        {User.VerifyHash
-          ? "Please verify your account as soon as possible, check your email for further information."
-          : "Thanks for verifying and becoming a full user."}
-      </p>
+      <p>Welcome to Community Classroom.</p>
+      {User.VerifyHash ? (
+        <div className="alert alert-danger">
+          <h3>Unverified Account</h3>
+          <p>
+            Please verify your account as soon as possible, check your email for
+            further information. Alternatively, if you know the Verification
+            Code, please enter here.
+          </p>
+          <form action={"/api/auth/verify/" + User.Username}>
+            <FormGroup
+              Id="Code"
+              Label="Verification Code"
+              Type="text"
+              Desc="Please enter the verification code you received in your email."
+            />
+            <input type="submit" className="btn btn-primary" value="Verify" />
+          </form>
+        </div>
+      ) : (
+        "Thanks for verifying and becoming a full user."
+      )}
       <button className="btn btn-danger" onClick={handleLogout}>
         Logout
       </button>
