e6ae4b6c5bcde5c0a456e80374e9a9d7ff6c33f8
Praveen Kumar <praveenscience@gmail.com>
2021-09-29 17:35:01 +0100

    Added a message to fill in personal & social profile details.

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -61,7 +61,13 @@ const Dashboard = ({ Card, handleLogout, User }) => {
           </form>
         </div>
       ) : (
-        "Thanks for verifying and becoming a full user."
+        <>
+          <p>Thanks for verifying and becoming a full user.</p>
+          {(Object.values(User.Personal).filter(v => v === null).length > 0 ||
+            Object.values(User.Socials).filter(v => v === null).length > 0) && (
+            <p>Please fill in all your profile details.</p>
+          )}
+        </>
       )}
       <button className="btn btn-danger" onClick={handleLogout}>
         Logout
