ad81bf350c1d1321ef58057841feaef8ee4f97b1
Praveen Kumar <praveenscience@gmail.com>
2021-09-29 17:27:19 +0100

    Sent Verification code to Backend from Dashboard.

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,23 +1,61 @@
+import { useState } from "react";
+import { VerifyUser } from "../../services/Auth";
 import FormGroup from "../Bootstrap/FormGroup";
 
 const Dashboard = ({ Card, handleLogout, User }) => {
+  const [Code, setCode] = useState("");
+  const [Output, setOutput] = useState(null);
+  const handleVerifyUser = e => {
+    e.preventDefault();
+    VerifyUser(User.Username, Code)
+      .then(res => {
+        if (res.status === 200) {
+          setOutput(
+            "Verification was successful. Please relogin to enable all the features."
+          );
+          setTimeout(() => {
+            handleLogout({ preventDefault: () => {} });
+          }, 2500);
+        }
+      })
+      .catch(() => {
+        setOutput("Error! Invalid Code.");
+      });
+  };
   return (
     <Card Title={`Welcome ${User.Name}`}>
       <p>Welcome to Community Classroom.</p>
       {User.VerifyHash ? (
         <div className="alert alert-danger">
           <h3>Unverified Account</h3>
+          {Output && (
+            <div
+              className={
+                "alert alert-" +
+                (Output.indexOf("Error") === 0 ? "dark" : "success")
+              }
+            >
+              {Output}
+            </div>
+          )}
           <p>
             Please verify your account as soon as possible, check your email for
             further information. Alternatively, if you know the Verification
             Code, please enter here.
           </p>
-          <form action={"/api/auth/verify/" + User.Username}>
+          <form
+            action={"/api/auth/verify/" + User.Username}
+            onSubmit={handleVerifyUser}
+          >
             <FormGroup
               Id="Code"
               Label="Verification Code"
               Type="text"
               Desc="Please enter the verification code you received in your email."
+              Value={Code}
+              onChange={e => {
+                setCode(e.target.value);
+              }}
             />
             <input type="submit" className="btn btn-primary" value="Verify" />
           </form>
--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -7,3 +7,6 @@ export const RegisterUser = (Username, Password, Name, Email, Role) =>
   Axios.post("/api/auth/register", { Username, Password, Name, Email, Role });
 
 export const LogoutUser = () => Axios.post("/api/auth/logout");
+
+export const VerifyUser = (Username, Code) =>
+  Axios.get(`/api/auth/verify/${Username}?Code=${Code}`);
