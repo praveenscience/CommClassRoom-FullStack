386327c9b699393753f1f84cb831f67940cfd8bb
Praveen Kumar <praveenscience@gmail.com>
2021-10-05 16:38:34 +0100

    Implemented React Router DOM in the Client App.

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -7,7 +8,9 @@ import "./styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
