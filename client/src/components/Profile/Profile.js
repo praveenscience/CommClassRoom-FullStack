8533f72485f881fd2b9a51da550b06972b42c720
Praveen Kumar <praveenscience@gmail.com>
2021-10-05 17:19:05 +0100

    Load the User Details from the API.
    * Store it in a local state using useState.
    * Get the data once every time the profile changes.
    * Using the useEffect Dependency Array.

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { GetUser } from "../../services/Users";
 
-const Profile = ({ Users, User, ...props }) => {
+const Profile = ({ match }) => {
+  const [User, setUser] = useState(null);
+  const Username = match.params.Username;
+  useEffect(() => {
+    if (Username) {
+      GetUser(Username).then(res => {
+        setUser(res.data);
+      });
+    }
+  }, [Username]);
   return (
     <div className="Profile">
-      <p>Hello</p>
-      <pre>{JSON.stringify({ props, Users, User }, null, 2)}</pre>
-      <p>This is User Path.</p>
+      {User ? (
+        <>
+          <h1>
+            User: {User.Name}
+            <span className="badge bg-primary Profile-Role">{User.Role}</span>
+          </h1>
+          <pre>{JSON.stringify(User, null, 2)}</pre>
+        </>
+      ) : (
+        <div className="text-center">
+          <p>Loading...</p>
+          <img src="https://i.imgur.com/5W7knyx.gif" alt="Loading..." />
+        </div>
+      )}
       <Link className="btn btn-success" to="/">
         Back to Dashboard
       </Link>
