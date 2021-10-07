5d104fe76539ccce8807afde8fd9e8298d26f229
Praveen Kumar <praveenscience@gmail.com>
2021-10-07 17:12:24 +0100

    Revamped Profile Page.

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { GetUser } from "../../services/Users";
+import DefaultAvatar from "../../assets/DefaultAvatar.png";
 
-const Profile = ({ match }) => {
+const Profile = ({ Card, match }) => {
   const [User, setUser] = useState(null);
   const Username = match.params.Username;
   useEffect(() => {
@@ -12,15 +13,105 @@ const Profile = ({ match }) => {
       });
     }
   }, [Username]);
+  let UserRows;
+  if (User) {
+    UserRows = [
+      {
+        Name: "Full Name",
+        Value: User.Name
+      },
+      {
+        Name: "Role",
+        Value: User.Role
+      },
+      {
+        Name: "Verification Status",
+        Value: User.VerifyHash ? "Not Verified" : "Verified"
+      },
+      {
+        Name: "Member From",
+        Value:
+          new Date(User.CreatedAt).toDateString() +
+          " at " +
+          new Date(User.CreatedAt).toLocaleTimeString().substr(0, 5)
+      }
+    ];
+    if (User.Institute) {
+      UserRows.push({
+        Name: "Phone Number",
+        Value: User.Institute
+      });
+    }
+    if (User.Personal.Gender) {
+      UserRows.push({
+        Name: "Gender",
+        Value: User.Personal.Gender
+      });
+    }
+    if (User.Personal.Phone) {
+      UserRows.push({
+        Name: "Phone Number",
+        Value: User.Personal.Phone
+      });
+    }
+    if (User.Personal.Location) {
+      UserRows.push({
+        Name: "Phone Number",
+        Value: User.Personal.Location
+      });
+    }
+  }
   return (
     <div className="Profile">
       {User ? (
         <>
-          <h1>
-            User: {User.Name}
-            <span className="badge bg-primary Profile-Role">{User.Role}</span>
-          </h1>
-          <pre>{JSON.stringify(User, null, 2)}</pre>
+          <Card Title={`Profile: ${User.Name}`} className="mb-3">
+            <div className="container">
+              <div className="row">
+                <div className="col-12 col-md-3">
+                  <div className="text-center my-4">
+                    <img
+                      src={User.Avatar ? User.Avatar : DefaultAvatar}
+                      className="img-thumbnail rounded-circle w-75"
+                      alt={User.Name}
+                    />
+                  </div>
+                </div>
+                <div className="col-12 col-md-9">
+                  <div className="list-group">
+                    {UserRows.map((row, key) => (
+                      <div className="list-group-item" key={key}>
+                        <small>{row.Name}</small>
+                        <div class="d-flex w-100 justify-content-between">
+                          <h5 class="mb-1">{row.Value}</h5>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Card>
+          {Object.keys(User.Socials).filter(social => User.Socials[social])
+            .length > 0 && (
+            <Card Title={`${User.Name}’s Socials`} className="mb-3">
+              <div className="col-12 col-md-9">
+                <div className="list-group">
+                  {Object.keys(User.Socials)
+                    .filter(social => User.Socials[social])
+                    .map(social => (
+                      <a
+                        href={User.Socials[social]}
+                        className="list-group-item list-group-item-action"
+                        key={social}
+                      >
+                        {social}
+                      </a>
+                    ))}
+                </div>
+              </div>
+            </Card>
+          )}
         </>
       ) : (
         <div className="text-center">
@@ -28,7 +119,7 @@ const Profile = ({ match }) => {
           <img src="https://i.imgur.com/5W7knyx.gif" alt="Loading..." />
         </div>
       )}
-      <Link className="btn btn-success" to="/">
+      <Link className="btn btn-success mb-4" to="/">
         Back to Dashboard
       </Link>
     </div>
